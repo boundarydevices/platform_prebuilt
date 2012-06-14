3d58ae9a8fc3e99db70cac538c16b36b7b85349f
Xinyu Chen <xinyu.chen@freescale.com>
2012-06-14 12:41:40 +0800

    ENGR00000002 Add busybox binary and makefile
    add busybox binary and makefile to let these tools in eng build.
    
    Signed-off-by: Zhang Jiejing <jiejing.zhang@freescale.com>

---
+++ android-arm/bin/Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := busybox
+LOCAL_MODULE_TAGS := eng
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_SRC_FILES := busybox
+include $(BUILD_PREBUILT)
